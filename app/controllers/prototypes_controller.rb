540c8ccb2b2b61862978591542174c8c1149748d
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-07-11 15:26:51 +0900

    一覧表示の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -62,7 +62,7 @@ class PrototypesController < ApplicationController
   private
   
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit( :title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
 end
